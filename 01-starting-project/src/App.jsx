de3edcfd2950850754940116f58334b1ecccb4b6
Anudeep Tadikamalla <sai.anudeep@idp.com>
2024-05-15 16:00:48 +0530

    Added results console log

--- 01-starting-project/src/App.jsx
+++ 01-starting-project/src/App.jsx
@@ -1,11 +1,31 @@
 import Header from "../src/components/header.jsx";
+import Results from "./components/Results.jsx";
 import UserInput from "./components/UserInput.jsx";
 
+import { useState } from "react";
+
 function App() {
+  const [userInput, setUserInput] = useState({
+    initialInvestment: 10000,
+    anualInvestment: 1000,
+    expectedReturn: 6,
+    duration: 10,
+  });
+
+  function ChnageHandler(inputIndetifier, newValue) {
+    setUserInput((prevUseInput) => {
+      return {
+        ...prevUseInput,
+        [inputIndetifier]: newValue,
+      };
+    });
+  }
+
   return (
     <>
       <Header />;
-      <UserInput />;
+      <UserInput userInput={userInput} onChange={ChnageHandler} />;
+      <Results input={userInput} />
     </>
   );
 }
--- 01-starting-project/src/components/header.jsx
+++ 01-starting-project/src/components/header.jsx
@@ -3,7 +3,7 @@ import logo from "../assets/logo.png";
 export default function Header() {
   return (
     <header id="header">
-      <img src={logo} alt="React logo" />
+      <img src={logo} alt="investment header logo" />
       <h1>React Investment caluclator</h1>
     </header>
   );
