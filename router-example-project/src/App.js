d39cce249dd4bf5f9a14421da00e99dfcdb1fa00
Anudeep Tadikamalla <sai.anudeep@idp.com>
2024-05-27 16:54:55 +0530

    Router exmaple project

--- router-example-project/src/App.js
+++ router-example-project/src/App.js
@@ -1,15 +1,42 @@
-import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import {
+  RouterProvider,
+  // createRoutesFromElements,
+  createBrowserRouter,
+} from "react-router-dom";
 import HomePage from "./pages/HomePage.js";
 import Product from "./pages/Product.js";
+import RootLayout from "./pages/Root.js";
+import Error from "./pages/Error.js";
+import ProductDetails from "./pages/ProductDetails.js";
+
+// const routedef = createRoutesFromElements(
+//   <Route>
+//     <Route path="/" element={<HomePage />} />,
+//     <Route path="/products" element={<Product />} />
+//   </Route>
+// );
+
+// const router = createBrowserRouter(routedef);
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <HomePage />,
-  },
-  {
-    path: "/products",
-    element: <Product />,
+    element: <RootLayout />,
+    errorElement: <Error />,
+    children: [
+      {
+        index: true,
+        element: <HomePage />,
+      },
+      {
+        path: "products",
+        element: <Product />,
+      },
+      {
+        path: "products/:productId",
+        element: <ProductDetails />,
+      },
+    ],
   },
 ]);
 
--- router-example-project/src/pages/HomePage.js
+++ router-example-project/src/pages/HomePage.js
@@ -1,5 +1,14 @@
+import { Link } from "react-router-dom";
+
 function HomePage() {
-  return <h1>My HomePage</h1>;
+  return (
+    <>
+      <h1>My HomePage</h1>
+      <p>
+        Go to <Link to="products"> Products </Link> page
+      </p>
+    </>
+  );
 }
 
 export default HomePage;
